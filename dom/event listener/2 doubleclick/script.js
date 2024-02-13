da3542dc06670c6e87f86e898ef9eb5cbac4c650
mdaatif11 <125191615+mdaatif11@users.noreply.github.com>
2024-02-14 01:34:01 +0530

    Add files via upload

---
+++ dom/event listener/2 doubleclick/script.js
@@ -0,0 +1,8 @@
+
+
+const boxElement = document.getElementById("box");
+
+// adding a dblclick event listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
+});
